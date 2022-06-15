e234e1df298be97da16608825f1db82a9c7e6667
HamzaAlwan <ihamzaalwan@gmail.com>
2022-06-15 03:30:09 +0300

    Initialized frontend

---
+++ frontend/apps/eshop/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot([])],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
