3a4d0edba8b6872fcad7749e816242be4236034b
HamzaAlwan <ihamzaalwan@gmail.com>
2022-07-04 05:47:06 +0300

    updated environment vars

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://i-eshop-backend.herokuapp.com//api/v1/',
+  apiUrl: 'https://i-eshop-backend.herokuapp.com/api/v1/',
   publicUrl: 'https://i-eshop-backend.herokuapp.com',
   stripePublishKey:
     'pk_test_51IBfUqKQtivAVU5Zf3emNj2DWwoAN0xXnERYBxrgRQcNpOokmZBitRRl5HOvAda0ACvsbffn9ZfKBXv85nEoEFst009LMTELt9',
